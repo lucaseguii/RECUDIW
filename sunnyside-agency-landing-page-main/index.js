7f501e46b99423ad9c918cb6ad56fa2df4faa936
lucaseguii <lsegui20216@iesjoanramis.org>
2025-03-28 14:52:15 +0100

    Primer commit al nou repositori

---
+++ sunnyside-agency-landing-page-main/index.js
@@ -0,0 +1,5 @@
+const mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+const navMenu = document.querySelector("#nav-menu");
+mobileMenuBtn.addEventListener("click", ()=>{
+    navMenu.classList.toggle("nav-visible");
+});
