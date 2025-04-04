bec5054ca9c7e98480c0d83a1d7f07cc00c9d655
lucaseguii <lsegui20216@iesjoanramis.org>
2025-04-04 17:20:43 +0200

    Superusuari i html de Gestio

--- FIREBASE/js/firebase.js
+++ FIREBASE/js/firebase.js
@@ -1,7 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
   import { getAuth, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"
-  import { getFirestore,collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
+  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
--- PIZZERIALUCA/js/firebase.js
+++ PIZZERIALUCA/js/firebase.js
@@ -1,7 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import { } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"
-  import { } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"
+  import { getFirestore, collection, addDoc} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,3 +22,27 @@
   const auth = getAuth(app);
   
   const db = getFirestore(app);
+
+
+  export const createSuperUsuari = async () =>{
+      const superUsuariEmail = "superusuari@gmail.com";
+      const superUsuariPassword = "super1234";
+
+      try{
+        const superUsuari = await signInWithEmailAndPassword(auth, superUsuariEmail, superUsuariPassword);
+        console.log("Existeix es superusuari")
+      }catch (error){
+        if (error.code === "auth/user-not-found") {
+          const superUsuari = await createUserWithEmailAndPassword(auth, superUsuariEmail, superUsuariPassword);
+
+          const docSuperUsuari = await addDoc(collection(db, "users"), {
+            email: superUsuariEmail
+          });
+          console.log("Superusuari creat", docSuperUsuari.email);
+          
+      } else {
+          console.log("error");
+      }
+  }
+}
+  
